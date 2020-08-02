6ebc2a99473d6f589d06fa895076463537a965a0
MartinKayz <kubonamartinyafesi132@gmail.com>
2020-08-02 21:14:30 +0300

    Added template dirs for accounts app

--- CustomerMgt/settings.py
+++ CustomerMgt/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'CustomerMgt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['accounts/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
