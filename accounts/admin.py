388057a462c8bdcca3a6453a94a6175430234a12
MartinKayz <kubonamartinyafesi132@gmail.com>
2020-08-13 15:15:16 +0300

    Added the Tag model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Customer,Product,Orders
+from .models import Customer,Product,Orders,Tag
 
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Orders)
+admin.site.register(Tag)
 
