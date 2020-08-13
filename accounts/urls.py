7e1e03ce10b64dcc40320b98b25ea5d3a07d5044
MartinKayz <kubonamartinyafesi132@gmail.com>
2020-08-13 22:21:42 +0300

    dynamic url to get customer by pk added

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("",views.home,name='accounts-home'),
     path("products/",views.products,name='products'),
-    path("customers/",views.customer,name='customer'),
+    path("customers/<str:pk>/",views.customer,name='customer'),
 
 ]
