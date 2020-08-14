d5f1db2f501019b2c92beb350c63618561992243
MartinKayz <kubonamartinyafesi132@gmail.com>
2020-08-14 15:18:33 +0300

    update and delete urls added

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path("",views.home,name='accounts-home'),
     path("products/",views.products,name='products'),
     path("customers/<str:pk>/",views.customer,name='customer'),
+    path('create_order/',views.createOrder,name="create_order"),
+    path('update_order/<str:pk>/',views.updateOrder,name="update_order"),
+    path('delete_order/<str:pk>/',views.deleteOrder,name="delete_order")
     
 
 ]
