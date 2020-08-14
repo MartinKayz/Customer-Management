afd42d7f2adce66b9eb90cd80e6289b8626865dd
MartinKayz <kubonamartinyafesi132@gmail.com>
2020-08-14 21:06:00 +0300

    modified name for customers url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path("",views.home,name='accounts-home'),
     path("products/",views.products,name='products'),
-    path("customers/<str:pk>/",views.customer,name='customer'),
-    path('create_order/',views.createOrder,name="create_order"),
+    path("customers/<str:pk>/",views.customer,name='customers'),
+    path('create_order/<str:pk>/',views.createOrder,name="create_order"),
     path('update_order/<str:pk>/',views.updateOrder,name="update_order"),
     path('delete_order/<str:pk>/',views.deleteOrder,name="delete_order")
     
